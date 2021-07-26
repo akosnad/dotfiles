2823db7075f4d9146174bac4de37708f56728c17
akosnad <akos@nadudvari.org>
2021-07-26 16:50:15 +0200

    setup: fixes
    Tested on a fresh system
    - Added zsh to required packages (don't ask why it wasn't there)
    - Fixed awesome startup script, made it generic to any mouse device

--- awesome/startup.sh
+++ awesome/startup.sh
@@ -11,7 +11,12 @@ xset +dpms
 xset r rate 150 50
 xset +fp ~/dotfiles/fonts/
 
-xinput set-prop 'pointer:Logitech G305' 'libinput Accel Speed' -0.8
+# xinput set-prop 'pointer:Logitech G305' 'libinput Accel Speed' -0.8
+for id in $(xinput --list | sed -n '/.*pointer/s/.*=\([0-9]\+\).*/\1/p')
+do
+    xinput set-prop $id "libinput Accel Speed" -0.8
+done
+
 setxkbmap -layout 'us,hu' -option "grp:caps_toggle"
 xmodmap -e "clear lock"
 
