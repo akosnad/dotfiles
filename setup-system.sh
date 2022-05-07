a2c9aa649e098dde2b28e0ad9f031565b352526c
akosnad <akos@nadudvari.org>
2022-05-07 13:33:19 +0200

    setup: fix system setup

--- setup-system.sh
+++ setup-system.sh
@@ -32,9 +32,9 @@ fi
 ### Chaotic aur
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key FBA220DFC880C036
-sudo pacman --needed -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman --needed --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 if ! grep -q -E "^\[chaotic-aur\]" /etc/pacman.conf; then
-    echo "[chaotic-aur]" >> /etc/pacman.conf
-    echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+    sudo sh -c 'echo "[chaotic-aur]" >> /etc/pacman.conf'
+    sudo sh -c 'echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf'
 fi
