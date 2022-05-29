60f91352eb66d150c659c603076ec42eb466b336
akosnad <akos@nadudvari.org>
2022-05-29 17:58:17 +0200

    added extra setup

---
+++ setup-extra.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+dotfiles="$(dirname $(realpath $BASH_SOURCE))"
+source "$dotfiles/setup-helpers.sh"
+
+$dotfiles/setup.sh
+
+### Packages
+verify_packages "$dotfiles/packages-extra"
+
+### Discord
+running_before=$(if (pidof Discord &>/dev/null); then echo 1; fi)
+if [[ $(betterdiscordctl status | grep "injected: no") ]]; then
+    betterdiscordctl install
+fi
+/usr/bin/discord </dev/null &>/dev/null &
+mkdir -p "$HOME/.config/beautifuldiscord"
+$HOME/.local/bin/beautifuldiscord --css "$HOME/.config/beautifuldiscord/style.css"
+if [[ ! $running_before == "1" ]]; then killall -INT Discord &>/dev/null; fi
+
