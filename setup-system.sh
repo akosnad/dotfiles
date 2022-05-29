35e4a1724296665bd793e0191ef7404fb15a5585
akosnad <akos@nadudvari.org>
2022-05-29 16:55:24 +0200

    system: added custom repo

--- setup-system.sh
+++ setup-system.sh
@@ -30,8 +30,10 @@ fi
 
 
 ### Chaotic aur
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
+if ! sudo pacman-key -l FBA220DFC880C036 &>/dev/null; then
+    sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+    sudo pacman-key --lsign-key FBA220DFC880C036
+fi
 if ! sudo pacman -Qi chaotic-keyring chaotic-mirrorlist &>/dev/null; then
     sudo pacman --needed --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 fi
@@ -39,4 +41,10 @@ fi
 if ! grep -q -E "^\[chaotic-aur\]" /etc/pacman.conf; then
     sudo sh -c 'echo "[chaotic-aur]" >> /etc/pacman.conf'
     sudo sh -c 'echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf'
+fi
+
+### Own aur build server
+if ! grep -q -E "^\[aurto\]" /etc/pacman.conf; then
+    sudo sh -c 'echo "[aurto]" >> /etc/pacman.conf'
+    sudo sh -c 'echo "Server = https://repo.xfzt.gq/arch" >> /etc/pacman.conf'
 fi
