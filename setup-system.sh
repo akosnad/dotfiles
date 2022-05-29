8611e6d63af04eef3586fc20f0c733843051cd10
akosnad <akos@nadudvari.org>
2022-05-29 17:13:09 +0200

    system: don't do signature checking on own repo

--- setup-system.sh
+++ setup-system.sh
@@ -46,5 +46,6 @@ fi
 ### Own aur build server
 if ! grep -q -E "^\[aurto\]" /etc/pacman.conf; then
     sudo sh -c 'echo "[aurto]" >> /etc/pacman.conf'
+    sudo sh -c 'echo "SigLevel = Never" >> /etc/pacman.conf'
     sudo sh -c 'echo "Server = https://repo.xfzt.gq/arch" >> /etc/pacman.conf'
 fi
