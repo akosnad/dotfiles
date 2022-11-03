63ef2a5bc2e5f6ace5e49237c527fc0720c9f5e4
akosnad <akos@nadudvari.org>
2022-11-03 19:44:06 +0100

    zsh: add aliases for managing cmake projects

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,1 +1,15 @@
 alias -g vim='nvim'
+alias n=ninja
+
+function cm(){
+    if pwd | grep -vqE "/build$"; then
+        if [ ! -f CMakeLists.txt ]; then
+            echo not a cmake project!
+            return
+        fi
+
+        if [ ! -d build ]; then mkdir build; fi
+        cd build
+    fi
+    cmake .. -DCMAKE_BUILD_TYPE=Debug -G Ninja -D "CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} -fdiagnostics-color=always"
+}
