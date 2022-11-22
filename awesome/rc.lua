a0439516e0a4112d20e45f397f6e43bdc2d035d8
akosnad <akos@nadudvari.org>
2022-11-22 14:05:32 +0100

    awesome: remember last used tab after a restart

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -400,6 +400,38 @@ awesome.connect_signal("reload", function()
     awesome.restart()
 end)
 
+-- Tag saving and restoring
+awesome.connect_signal('exit', function(reason_restart)
+   -- if not reason_restart then return end
+
+   local file = io.open(settings.tag_cachefile, 'w+')
+
+   for s in screen do
+      file:write(s.selected_tag.index, '\n')
+   end
+
+   file:close()
+end)
+
+awesome.connect_signal('startup', function()
+   local file = io.open(settings.tag_cachefile, 'r')
+   if not file then return end
+
+   local selected_tags = {}
+
+   for line in file:lines() do
+      table.insert(selected_tags, tonumber(line))
+   end
+
+   for s in screen do
+      local i = selected_tags[s.index]
+      local t = s.tags[i]
+      t:view_only()
+   end
+
+   file:close()
+end)
+
 -- }}
 
 awful.util.spawn_with_shell("light-locker --lock-after-screensaver=0")
--- awesome/settings.lua
+++ awesome/settings.lua
@@ -1,12 +1,13 @@
 local settings = {
-    terminal     = "urxvt",
-    vi_focus     = false, -- vi-like client focus https://github.com/lcpz/awesome-copycats/issues/275
-    cycle_prev   = true,  -- cycle with only the previously focused client or all https://github.com/lcpz/awesome-copycats/issues/274
-    editor       = os.getenv("EDITOR") or "nvim",
-    browser      = "google-chrome-stable",
-    scrlocker    = "sh -c 'sleep 2; light-locker-command -l'",
---    scrlocker    = "xset dpms force off",
-    screenshot   = "flameshot gui",
+    terminal      = "urxvt",
+    vi_focus      = false, -- vi-like client focus https://github.com/lcpz/awesome-copycats/issues/275
+    cycle_prev    = true,  -- cycle with only the previously focused client or all https://github.com/lcpz/awesome-copycats/issues/274
+    editor        = os.getenv("EDITOR") or "nvim",
+    browser       = "google-chrome-stable",
+    scrlocker     = "sh -c 'sleep 2; light-locker-command -l'",
+--    scrlocker     = "xset dpms force off",
+    screenshot    = "flameshot gui",
+    tag_cachefile = "/tmp/awesome_tag_cache"
 }
 
 return settings
