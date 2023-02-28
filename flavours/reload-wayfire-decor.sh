83a8bd3011afb1b0d709596f3c4fa00e6537097a
akosnad <akos@nadudvari.org>
2023-02-28 16:22:20 +0100

    flavours: remove wayfire config

--- flavours/reload-wayfire-decor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-active_color=$(grep -P "^active_color" $HOME/.config/wayfire-win-decor)
-inactive_color=$(grep -P "^inactive_color" $HOME/.config/wayfire-win-decor)
-
-sed -i "s/^active_color.*/${active_color}/" $HOME/.config/wayfire.ini
-sed -i "s/^inactive_color.*/${inactive_color}/" $HOME/.config/wayfire.ini
