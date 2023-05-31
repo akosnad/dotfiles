bae28690e616cdebc5b928ea72f9ff84fb77bc66
akosnad <akos@nadudvari.org>
2023-05-31 23:00:06 +0200

    waydroid: ensure waydroid-launch is in sessions list

--- waydroid/setup.sh
+++ waydroid/setup.sh
@@ -8,3 +8,14 @@ sudo install -m 755 -o root -g root ./waydroid-launch /usr/local/bin
 sudo mkdir -p /etc/waydroid
 sudo install -m 644 -o root -g root ./sway-config /etc/waydroid/sway-config
 sudo install -m 755 -o root -g root ./*.sh /etc/waydroid/
+
+w="waydroid-launch"
+envs="/etc/greetd/environments"
+
+if ! [ -d "$(dirname $envs)" ]; then
+    sudo mkdir -p "$(dirname $envs)"
+fi
+filter=$(echo $w | sed 's/\-/\\\-/g')
+if ! grep -q "$filter" "$envs" >/dev/null 2>&1; then
+    sudo sh -c "echo $w >> $envs"
+fi
