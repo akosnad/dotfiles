9ccc83f3a8fe5c8107cf4993b960345c93e6ebac
akosnad <akos@nadudvari.org>
2023-12-04 14:51:09 +0100

    vim: magit: add keymap for vimagit and lazy load it

--- vim/plugins/git.lua
+++ vim/plugins/git.lua
@@ -1,6 +1,13 @@
 return {
     {
         'jreybert/vimagit',
+        cmd = 'Magit',
+        keys = { '<leader>m' },
+        config = function()
+            vim.keymap.set('n', '<leader>m', function()
+                vim.fn.execute('Magit')
+            end, { noremap = true })
+        end
     },
     {
         'tpope/vim-fugitive',
