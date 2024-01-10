d345de59a2a3cf1e1015502383828463514ac6b1
akosnad <akos@nadudvari.org>
2024-01-11 00:04:46 +0100

    vim: bind FZF to `<leader>F`

--- vim/plugins/fzf.lua
+++ vim/plugins/fzf.lua
@@ -10,9 +10,13 @@ return {
         'junegunn/fzf.vim',
         lazy = true,
         cmd = "FZF",
+        keys = { '<leader>F' },
         dependencies = {
             'junegunn/fzf',
         },
+        config = function()
+            vim.keymap.set('n', '<leader>F', ':FZF<CR>', { silent = true, noremap = true })
+        end,
     },
     {
         'antoinemadec/coc-fzf',
